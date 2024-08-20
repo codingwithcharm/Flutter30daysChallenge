35fb1c2d42edf82057090f49606ac28e8ef2a4fa
codingwithcharm <cvora0007@gmail.com>
2024-08-20 09:29:08 +0530

    inital

---
+++ android/app/src/main/java/com/example/thirty_days_challenge/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.thirty_days_challenge;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('DAY 1'),
+        ),
+        body: const Center(child: Text("How are You...!!")),
+      ),
+    ),
+  );
+}
